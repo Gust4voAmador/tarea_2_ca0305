931f8de5fbcba919d53e5f6652b1811a73747d8f
Gust4voAmador <gustavoamador187@gmail.com>
2024-04-14 18:44:36 -0600

    Feat: se agrega la función de contar letras en ConteoTexo
    La funcón no muestra las palabras ni las letras en orden en sus diccionarios

--- ConteoTexto.py
+++ ConteoTexto.py
@@ -30,9 +30,44 @@ def conteo_palabras(frase):
 #print(conteo)
 
 
-
+def conteo_letras(frase):
+    #Obtener lista con las palabra de la frase sin repetidas
+    set_palabras = list(set(frase.split()))
     
+    dicc_grande = {}
+    
+    #for que recorra el set de palabras
+    for palabra in set_palabras:
+        dicc_palabra = {} 
+        
+        #crear una lista con las letras sin repetir
+        lista_letras = list(palabra)
+        set_letras = list(set(lista_letras))
+        
+        #for que recorra las letras sin repetir
+        for letra in set_letras:
+            
+            contador = 0
+            #for que cuente las veces que aparece cada letra
+            for caracter in lista_letras:
+                
+                #condición para que cuente
+                if letra == caracter:
+                    contador = contador + 1
+                    
+            #agregar letra y respectivas apariciones en la palabra       
+            dicc_palabra[letra] = contador   
+            
+        #agragar diccionario respectivo a cada palabra con las letras repetidas  
+        dicc_grande[palabra] = dicc_palabra
 
+
+    return dicc_grande
+
+
+pepe = conteo_letras('Holaoooooo oo mundu')
+
+print(pepe)
 
 
 
